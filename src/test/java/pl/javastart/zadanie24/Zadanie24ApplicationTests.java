cc2c377fa88d139f57ca8a262e148763805a187d
Bartosz Nieszporek <bartosz.nieszporek@interia.pl>
2024-12-10 00:08:24 +0100

    initial commit

---
+++ src/test/java/pl/javastart/zadanie24/Zadanie24ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.javastart.zadanie24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zadanie24ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
